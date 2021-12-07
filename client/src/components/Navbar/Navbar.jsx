18315d2b233051929d70c151ca6beda823482013
jlp21 <portelli21@gmail.com>
2021-12-06 22:23:39 -0500

    Added to Navbar and cleared home page

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -12,6 +12,14 @@ class Navbar extends Component {
                 <Link to='/'>
                     Home
                 </Link>
+                
+                <Link to ='/about'>
+                    About
+                </Link>
+
+                <Link to='/projects'>
+                    Projects
+                </Link>
             </navbar>
         )
     }
